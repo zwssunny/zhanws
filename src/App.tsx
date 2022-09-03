db5bcb8aa91991c45fbee3f244d71ae9cdb5a2bf
jackson <zhanws@vip.163.com>
2022-09-03 17:19:52 +0800

    修改首页内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import fstLogo from './fstlogo.png';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={fstLogo} className="foresight-logo" alt="foresight-logo" />
+        <span className="Big-plus">+</span>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to my dweb.
